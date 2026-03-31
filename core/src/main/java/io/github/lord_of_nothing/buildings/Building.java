817b3d2c7496bf831039997337c59d85a3b67c9e
s3005756 <s3005756@ba-sachsen.de>
2026-03-31 13:55:41 +0200

    logic without asset

--- core/src/main/java/io/github/lord_of_nothing/buildings/Building.java
+++ core/src/main/java/io/github/lord_of_nothing/buildings/Building.java
@@ -1,5 +1,7 @@
 package io.github.lord_of_nothing.buildings;
+
 import io.github.lord_of_nothing.resources.ResourceType;
+
 import java.util.EnumMap;
 import java.util.Map;
 
@@ -7,11 +9,43 @@ import java.util.Map;
  * Defines the requirements for a building, via mapping.
  */
 public abstract class Building {
+    private final int width;
+    private final int height;
+    private int rootX, rootY;
+
+    /**
+     * <summary>Returns a unique identifier for the building type to resolve textures generically.</summary>
+     */
+    public abstract String getBuildingTypeKey();
+
     protected final Map<ResourceType, Integer> costs = new EnumMap<>(ResourceType.class);
 
     public Map<ResourceType, Integer> getCosts() {
         return costs;
     }
 
+    public Building(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
     public abstract String getTexturePath();
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setRootPosition(int x, int y) {
+        this.rootX = x;
+        this.rootY = y;
+    }
+
+    public boolean isRoot(int x, int y) {
+        return this.rootX == x && this.rootY == y;
+    }
+
 }
--- core/src/main/java/io/github/lord_of_nothing/grid/Grid.java
+++ core/src/main/java/io/github/lord_of_nothing/grid/Grid.java
@@ -1,5 +1,7 @@
 package io.github.lord_of_nothing.grid;
 
+import io.github.lord_of_nothing.buildings.Building;
+
 public class Grid {
     private static final int width = 32;
     private static final int height = 18;
@@ -42,11 +44,36 @@ public class Grid {
     public int getHoveredY() { return hoveredY; }
 
     /**
-     * Platziert ein beliebiges Gebäude-Objekt auf den angegebenen Koordinaten.
+     * Places any buildng on set coordinates.
      */
     public void setBuilding(int x, int y, io.github.lord_of_nothing.buildings.Building building) {
         if (isInside(x, y)) {
             tiles[x][y].setBuilding(building);
+        }
+    }
+
+    /**
+     * <summary>Checks if a building of given dimensions can be placed at the target coordinates.</summary>
+     * @param x Start X-coordinate. @param y Start Y-coordinate. @param w Width. @param h Height.
+     */
+    public boolean canPlace(int x, int y, int w, int h) {
+        for (int ix = x; ix < x + w; ix++) {
+            for (int iy = y; iy < y + h; iy++) {
+                if (!isInside(ix, iy) || getTile(ix, iy).hasBuilding()) return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * <summary>Places a building across multiple tiles and sets the root reference for rendering.</summary>
+     */
+    public void placeBuilding(int x, int y, Building b) {
+        b.setRootPosition(x, y);
+        for (int ix = x; ix < x + b.getWidth(); ix++) {
+            for (int iy = y; iy < y + b.getHeight(); iy++) {
+                tiles[ix][iy].setBuilding(b);
+            }
         }
     }
 }
