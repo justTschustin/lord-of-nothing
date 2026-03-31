3b9110727c3827362d4f4069981b12582ff6c590
Justin Hesse <justin.hesse@sandstorm.de>
2026-03-31 14:17:08 +0200

    :lipstick: Fullscreen mode fix MacOS desktop switch

--- lwjgl3/src/main/java/io/github/lord_of_nothing/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/io/github/lord_of_nothing/lwjgl3/Lwjgl3Launcher.java
@@ -1,5 +1,6 @@
 package io.github.lord_of_nothing.lwjgl3;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import io.github.lord_of_nothing.Main;
@@ -21,14 +22,12 @@ public class Lwjgl3Launcher {
         //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
         //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
         configuration.useVsync(true);
-        //// Limits FPS to the refresh rate of the currently active monitor, plus 1 to try to match fractional
-        //// refresh rates. The Vsync setting above should limit the actual FPS to match the monitor.
-        configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
+
         //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
+        //configuration.setWindowedMode(1080, 720);
 
-        configuration.setWindowedMode(1080, 720);
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
         //// They can also be loaded from the root of assets/ .
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
@@ -44,8 +43,14 @@ public class Lwjgl3Launcher {
 //        configuration.setOpenGLEmulation(Lwjgl3ApplicationConfiguration.GLEmulation.ANGLE_GLES20, 0, 0);
 
         configuration.setForegroundFPS(30);
-        configuration.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+
+        Graphics.DisplayMode displayMode = Lwjgl3ApplicationConfiguration.getDisplayMode();
+
+        configuration.setAutoIconify(false);
+        configuration.setFullscreenMode(displayMode);
         configuration.setWindowSizeLimits(1080, 720, 9999, 9999);
+        configuration.setPauseWhenMinimized(true);
+        configuration.setPauseWhenLostFocus(false);
 
         return configuration;
     }
