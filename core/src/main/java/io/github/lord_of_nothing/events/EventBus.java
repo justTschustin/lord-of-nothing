40a9ad202d13cc167a56a867243e5fc8a34254a9
Justin Hesse <justin.hesse@sandstorm.de>
2026-04-06 19:33:15 +0200

    :rotating_light: Fix linter

--- core/src/main/java/io/github/lord_of_nothing/events/EventBus.java
+++ core/src/main/java/io/github/lord_of_nothing/events/EventBus.java
@@ -2,7 +2,6 @@ package io.github.lord_of_nothing.events;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Consumer;
 
 public class EventBus {
