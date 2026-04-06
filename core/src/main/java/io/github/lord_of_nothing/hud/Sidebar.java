076b1d89047abb9fdbc4e813fdb79e0337b36509
valusz <ichbindervali@gmail.com>
2026-04-06 17:20:20 +0200

    Linter fix

--- core/src/main/java/io/github/lord_of_nothing/hud/Sidebar.java
+++ core/src/main/java/io/github/lord_of_nothing/hud/Sidebar.java
@@ -1,8 +1,11 @@
 package io.github.lord_of_nothing.hud;
 
-import com.badlogic.gdx.Gdx;
-import io.github.lord_of_nothing.GameWindow;
-import io.github.lord_of_nothing.buildings.*;
+import io.github.lord_of_nothing.buildings.Building;
+import io.github.lord_of_nothing.buildings.House;
+import io.github.lord_of_nothing.buildings.Field;
+import io.github.lord_of_nothing.buildings.Sawmill;
+import io.github.lord_of_nothing.buildings.Quarry;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,7 +33,7 @@ public class Sidebar {
      * <remarks>Iterates through all templates to find a vertical match within the sidebar's screen area.</remarks>
      */
     public Building getBuildingAt(float x, float y) {
-        if (x > io.github.lord_of_nothing.GameWindow.SIDEBAR_WIDTH) return null;
+        if (x > io.github.lord_of_nothing.GameWindow.SIDEBAR_WIDTH) {return null;}
 
         int startY = com.badlogic.gdx.Gdx.graphics.getHeight() - io.github.lord_of_nothing.GameWindow.TOP_BAR_HEIGHT;
 
