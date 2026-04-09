9e771a98400913cbf1cb0113423ddfce274eeec5
Justin Hesse <justin.hesse@sandstorm.de>
2026-04-09 19:01:10 +0200

    :rotating_light: Linter fixes

--- core/src/main/java/io/github/lord_of_nothing/Main.java
+++ core/src/main/java/io/github/lord_of_nothing/Main.java
@@ -16,7 +16,6 @@ import io.github.lord_of_nothing.events.OpenSettingsMenuEvent;
 import io.github.lord_of_nothing.events.PauseGameEvent;
 import io.github.lord_of_nothing.events.ResumeGameEvent;
 import io.github.lord_of_nothing.events.StartGameEvent;
-import io.github.lord_of_nothing.events.ToggleFullscreenEvent;
 import io.github.lord_of_nothing.flow.FlowState;
 import io.github.lord_of_nothing.flow.GameplayFlowCoordinator;
 import io.github.lord_of_nothing.flow.MenuFlowCoordinator;
