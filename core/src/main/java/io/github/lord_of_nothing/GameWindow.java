c2fa27c22d51e18b31dbfafef9d427e00e3cdec7
Justin Hesse <justin.hesse@sandstorm.de>
2026-04-09 20:23:12 +0200

    :fire: Remove deprecated close button methods

--- core/src/main/java/io/github/lord_of_nothing/GameWindow.java
+++ core/src/main/java/io/github/lord_of_nothing/GameWindow.java
@@ -115,19 +115,6 @@ public class GameWindow {
     public int getTopBarY() { return Gdx.graphics.getHeight() - TOP_BAR_HEIGHT; }
 
     /**
-     * Delivers the X-Position of the close button.
-     */
-
-    public int getCloseButtonX() { return Gdx.graphics.getWidth() - CLOSE_BUTTON_SIZE - CLOSE_BUTTON_MARGIN; }
-
-    /**
-     * Delivers the Y-Position of the close button.
-     */
-    public int getCloseButtonY() {
-        return getTopBarY() + (TOP_BAR_HEIGHT - CLOSE_BUTTON_SIZE) / 2;
-    }
-
-    /**
      * Calculates dimensions for the right info panel using relative screen percentages
      */
     public float getRightSidebarWidth() { return Gdx.graphics.getWidth() * 0.20f; }
