2575b7cb3998c6c233617e35a757b4992ab5b4e1
Valentin Marcinek <valentin.marcinek@semodia.com>
2026-04-13 15:06:53 +0200

    Merge remote-tracking branch 'origin/feature/assignVillager' into feature/assignVillager

--- core/src/main/java/io/github/lord_of_nothing/buildings/Quarry.java
+++ core/src/main/java/io/github/lord_of_nothing/buildings/Quarry.java
@@ -3,7 +3,7 @@ package io.github.lord_of_nothing.buildings;
 import io.github.lord_of_nothing.resources.ResourceType;
 
 /**
- * Initializes a large-scale stone extraction site with a 2x2 footprint on the grid and costs of 30 wood.
+ * Initializes a large-scale stone extraction site with a 4x4 footprint on the grid and costs of 30 wood.
  */
 public class Quarry extends Building {
     public Quarry() {
