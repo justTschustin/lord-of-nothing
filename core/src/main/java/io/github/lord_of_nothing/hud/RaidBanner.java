1b1ad5c763e3095ffb20d4df3648a27452ddf149
Christopher Sabbach <christopher.sabbach@sandstorm.de>
2026-05-10 13:24:11 +0200

    delete unused import

--- core/src/main/java/io/github/lord_of_nothing/hud/RaidBanner.java
+++ core/src/main/java/io/github/lord_of_nothing/hud/RaidBanner.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 /**
  * Animated banner that overlays grid during raid warnings and attacks, and outcome text
